5fa9d79ee1594ed6f4d1e229abe579f158e66dd3
Shreyas Rao <raoshreyas2004@gmail.com>
2024-01-23 22:37:56 -0500

    Added 3 more endpoints for basic CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from "express";
-import { MongoClient, ObjectId } from "mongodb";
+import { Collection, MongoClient, ObjectId } from "mongodb";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { decode } from "jsonwebtoken";
 
 const app = express();
 const PORT = 4000;
 const mongoURL = "mongodb://localhost:27017";
 const dbName = "quirknotes";
 
-// Connect to MongoDB
+// Middleware for the request processing pipeline
+app.use(express.json());
+
+// Variable to connect to MongoDB
 let db;
 
 async function connectToMongo() {
@@ -35,10 +38,10 @@ app.listen(PORT, () => {
 const COLLECTIONS = {
     notes: "notes",
     users: "users",
-  };
+};
 
 // Register a new user
-app.post("/registerUser", express.json(), async (req, res) => {
+app.post("/registerUser", async (req, res) => {
     try {
       const { username, password } = req.body;
   
@@ -56,24 +59,25 @@ app.post("/registerUser", express.json(), async (req, res) => {
         return res.status(400).json({ error: "Username already exists." });
       }
   
-      // Creating hashed password (search up bcrypt online for more info)
-      // and storing user info in database
+      // Creating hashed password using bcrypt and storing user info in database
       const hashedPassword = await bcrypt.hash(password, 10);
       await userCollection.insertOne({
         username,
         password: hashedPassword,
       });
   
-      // Returning JSON Web Token (search JWT for more explanation)
+      // Returning JSON Web Token using JWT
       const token = jwt.sign({ username }, "secret-key", { expiresIn: "1h" });
       res.status(201).json({ response: "User registered successfully.", token });
+
+      // res.json({ response: `Here's your username: ${username}` }); // Implicite status 200
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  });
+});
 
 // Log in an existing user
-app.post("/loginUser", express.json(), async (req, res) => {
+app.post("/loginUser", async (req, res) => {
     try {
       const { username, password } = req.body;
   
@@ -100,10 +104,10 @@ app.post("/loginUser", express.json(), async (req, res) => {
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  });
+});
 
 // Post a note belonging to the user
-app.post("/postNote", express.json(), async (req, res) => {
+app.post("/postNote", async (req, res) => {
     try {
       // Basic body request check
       const { title, content } = req.body;
@@ -135,10 +139,10 @@ app.post("/postNote", express.json(), async (req, res) => {
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  });
+});
 
 // Retrieve a note belonging to the user
-app.get("/getNote/:noteId", express.json(), async (req, res) => {
+app.get("/getNote/:noteId", async (req, res) => {
     try {
       // Basic param checking
       const noteId = req.params.noteId;
@@ -169,4 +173,123 @@ app.get("/getNote/:noteId", express.json(), async (req, res) => {
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  });
+});
+
+// Retrieving all notes from the Notes collections
+app.get("/getAllNotes", async (req, res) => {
+  try {
+    // Validation
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, "secret-key", async (err, decoded) => {
+      if (err) {
+        return res.status(401).send("Unauthorized.");
+      }
+
+      // Find all notes beloing to the user
+      // query by username
+      const collection = db.collection(COLLECTIONS.notes);
+      const data = await collection.find({
+        username: decoded.username,
+      });
+
+
+      let allNotes = [];
+      for await (const note of data) {
+        allNotes.push(note);
+      }
+
+      res.json({ response: allNotes}); // implicit status 200
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete a particular note from Notes collections
+app.delete("/deleteNote/:noteId", async (req, res) => {
+  try {
+    const noteId = req.params.noteId;
+    if (!ObjectId.isValid(noteId)) {
+      return res.status(400).json({ error: "Invalid note ID." });
+    }
+
+    // Validation
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, "secret-key", async (err, decoded) => {
+      if (err) {
+        return res.status(401).send("Unauthorized.");
+      }
+
+      const collection = db.collection(COLLECTIONS.notes);
+      // deleting the requested note
+      const data = await collection.deleteOne({ 
+        _id : new ObjectId(noteId), 
+        username: decoded.username, 
+      });
+
+      if (!data || data.deletedCount == 0) {
+        return res.status(404).json({ error: `Note with ID ${noteId} belonging to the user not found` });
+      }
+
+      res.json({ response: `Document with ID ${noteId} properly deleted.` });
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Editing the note document in the Notes collection
+app.patch("/editNote/:noteId", async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const noteId = req.params.noteId;
+
+    // Validation of the data receieved
+    if (!title && !content) {
+      return res.status(400).json({ error: "Invalid body paramamters." });
+    } 
+
+    if (!ObjectId.isValid(noteId)) {
+      return res.status(400).json({ error: "Invalid note ID." });
+    }
+    
+
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, "secret-key", async (err, decoded) => {
+      if (err) {
+        return res.status(401).send("Unauthorized.");
+      }
+
+      const collection = db.collection(COLLECTIONS.notes);
+
+      // Retrieve document if it exists
+      const data = await collection.findOne({
+        username: decoded.username,
+        _id: new ObjectId(noteId),
+      });
+
+      // Validate the document
+      if (!data) {
+        return res.status(404).json({ error: `Note with ID ${noteId} belonging to the user not found` });
+      }
+
+      // Set the attributes that may change
+      const updateDoc = {
+        $set: {
+          title: title ? title : data.title,
+          content: content ? content : data.content, 
+        }
+      }
+
+      // Update the document 
+      const updateData = await collection.updateOne({ _id: new ObjectId(noteId), username: decoded.username }, updateDoc);
+
+      if (!updateData || updateData.matchedCount == 0) {
+        return res.status(404).json({ error: `Note with ID ${noteId} belonging to the user not found.` })
+      }
+      res.json({ response: `Document with ID ${noteId} properly updated`, data: updateData }); // implicit status 200
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
